1453fdefe76739c204dd4f39bdfbca3221856be6
PixelBoom <rithikbhatia12@gmail.com>
2022-01-12 21:09:05 +0400

    Made leaderboard set interface public

--- LeaderboardCore/Interfaces/INotifyLeaderboardSet.cs
+++ LeaderboardCore/Interfaces/INotifyLeaderboardSet.cs
@@ -1,7 +1,14 @@
 ﻿namespace LeaderboardCore.Interfaces
 {
-    interface INotifyLeaderboardSet
+    /// <summary>
+    /// Implement this interface to be notified when a map is selected.
+    /// </summary>
+    public interface INotifyLeaderboardSet
     {
+        /// <summary>
+        /// Called when a map has been selected on the leaderboard.
+        /// <param name="difficultyBeatmap"></param>
+        /// </summary>
         public void OnLeaderboardSet(IDifficultyBeatmap difficultyBeatmap);
     }
 }
